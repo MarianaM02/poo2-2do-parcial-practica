cde85d1464d0024dd3b93635de849fb2250c76b6
AlvaradoRocio <71291854+AlvaradoRocio@users.noreply.github.com>
2022-06-17 11:20:49 -0300

    cambio.py
    Agregado De clases que no funciona, creo que asi no tiraba error pero no sale el out

--- cambio.py
+++ cambio.py
@@ -1,50 +1,60 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-archivo = open("cambio.in", "rt")
-linea1 = archivo.readline().split()
-cantidadEsquinas = int(linea1[0])
-origenColectivo = int(linea1[1])
-destinoEscuela = int(linea1[2])
+def main():
+    archivo = open("cambio.in", "rt")
+    linea1 = archivo.readline().split()
+    cantidadEsquinas = int(linea1[0])
+    origenColectivo = int(linea1[1])
+    destinoEscuela = int(linea1[2])
 
-cantidadCalles = int(archivo.readline())
+    cantidadCalles = int(archivo.readline())
 
-calles = []
-for x in archivo:
-    calles.append(tuple(int(i) for i in x.split()))
-archivo.close()
+    calles = []
+    for x in archivo:
+        calles.append(tuple(int(i) for i in x.split()))
+    archivo.close()
+    oCambioCallesMano = CambioCallesMano(calles, origenColectivo, destinoEscuela)
 
-barrio = nx.Graph()
-barrio.add_weighted_edges_from(calles)
+class CambioCallesMano:
+    def __init__(self, calles, origenColectivo, destinoEscuela):
+        self.calles=calles
+        self.origenColectivo=origenColectivo
+        self.destinoEscuela=destinoEscuela
+        self.barrio = nx.Graph()
+        self.barrio.add_weighted_edges_from(calles)
 
-barrioOriginal = nx.DiGraph()
-barrioOriginal.add_weighted_edges_from(calles)
+        self.barrioOriginal = nx.DiGraph()
+        self.barrioOriginal.add_weighted_edges_from(calles)
 
-distancias, caminos = nx.single_source_dijkstra(barrio, origenColectivo)
-camino = caminos[destinoEscuela]
-distancia = distancias[destinoEscuela]
+        self.distancias, caminos = nx.single_source_dijkstra(barrio, origenColectivo)
+        self.camino = caminos[destinoEscuela]
+        self.distancia = distancias[destinoEscuela]
+        encontrarCallesACambiar()
 
-callesACambiar = []
-for i in range(len(camino)-1):
-    actual = camino[i]
-    siguiente = camino[i+1]
-    adyacentes = nx.neighbors(barrioOriginal, actual)
-    if siguiente not in adyacentes:
-        for i in range(len(calles)):
-            if calles[i][0] == siguiente and calles[i][1] == actual:
-                callesACambiar.append(i+1)
-                continue
+    def encontrarCallesACambiar():
+        callesACambiar = []
+        for i in range(len(self.camino)-1):
+            actual = self.camino[i]
+            siguiente = self.camino[i+1]
+            adyacentes = nx.neighbors(self.barrioOriginal, actual)
+            if siguiente not in adyacentes:
+                for i in range(len(self.calles)):
+                    if calles[i][0] == siguiente and self.calles[i][1] == actual:
+                        callesACambiar.append(i+1)
+                        continue
 
-callesACambiar.sort()
-
-archivo = open("cambio.out", "wt")
-archivo.write(str(distancia) + "\n")
-archivo.write("".join(str(i) + " " for i in callesACambiar))
-archivo.close()
+        callesACambiar.sort()
+        imprimirArchivo()
+    def imprimirArchivo():        
+        archivo = open("cambio.out", "wt")
+        archivo.write(str(distancia) + "\n")
+        archivo.write("".join(str(i) + " " for i in callesACambiar))
+        archivo.close()
 
 
 # mostrar grafo
 """ pos = nx.spring_layout(barrio)
 nx.draw(barrio, pos, with_labels=True)
 
-plt.show() """
+plt.show()"""
