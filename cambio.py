d2cbb4a10fcae8c0dcc986c85134104960fb8f0f
Marian <mrn.m.92@gmail.com>
2022-06-17 14:00:04 -0300

    Cambiados nombre Casos de prueba

--- cambio.py
+++ cambio.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def main():
     try:
-        archivo = open("cambio_caso3_todos_cambios.in", "rt")
+        archivo = open("caso_06_salida_noHayCambios.in", "rt")
         linea1 = archivo.readline().split()
         cantidadEsquinas = int(linea1[0])
         origenColectivo = int(linea1[1])
